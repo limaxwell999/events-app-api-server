bac18ed98b8fdcb19ae3dbf5794580ebb0f3e57d
limaxwell999 <limaxwell@deloitte.com>
2022-11-03 17:05:07 +0000

    Changed Team Name

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Team 6';
+    team = 'Team 689';
     version = '1.0.0';
 }
 module.exports = AppConfig;
